469647b4ca7199191f6e12f386cf4d6d9887891e
RomanVolobuev54 <roma3209@gmail.com>
2024-10-24 09:53:05 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,11 @@
+#"1st program"
+print(5*(9**0.5))
+#"2nd program"
+print(9.99>9.98 and 1000 != 1000.1)
+#"3rd program"
+print(2*2+2)
+print(2*(2+2))
+print(6==8)
+#"4th program"
+print(type(float('123.456')))
+print((int(float('123.456')*10))%10)
